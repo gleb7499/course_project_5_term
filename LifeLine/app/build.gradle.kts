bd21b257ffbddc8052b095cb2cceb99b863102a3
gleb7499 <164905074+gleb7499@users.noreply.github.com>
2024-11-15 12:14:10 +0300

    Initialed project
     - With involve Sonar

---
+++ LifeLine/app/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    alias(libs.plugins.android.application)
+    id("org.sonarqube") version "5.1.0.4882"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "gleb7499_course-project-5-term")
+        property("sonar.organization", "gleb7499")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
+
+android {
+    namespace = "com.example.lifeline"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.lifeline"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
